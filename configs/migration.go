47466c804c1cbede5af3783a23bac299f82a4174
Alim Mughanil Karim <alim.mughanil23@gmail.com>
2023-09-29 14:46:11 +0700

    feat(products): add crud for products (#8)

--- configs/migration.go
+++ configs/migration.go
@@ -2,10 +2,12 @@ package configs
 
 import (
 	CategoryModel "batch11_golang_pos/models/category"
+	ProductModel "batch11_golang_pos/models/product"
 	UserModel "batch11_golang_pos/models/user"
 )
 
 func initMigrate() {
 	DB.AutoMigrate(&UserModel.User{})
 	DB.AutoMigrate(&CategoryModel.Category{})
+	DB.AutoMigrate(&ProductModel.Product{})
 }
